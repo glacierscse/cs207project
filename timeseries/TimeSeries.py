892558ceba8b2d9074be99304cefd6dd37f7c4e8
JunZhou1544 <judyzhou0201@gmail.com>
2016-10-15 16:19:54 -0400

    fixed the bug

--- timeseries/TimeSeries.py
+++ timeseries/TimeSeries.py
@@ -1,15 +1,19 @@
+import numbers, reprlib
 class TimeSeries:
     # TimeSeries class	
     def __init__(self,data):
-        self.timeSeries = data
+        self._time_series = data
     # get the length of data
     def __len__(self):
-        return len(data)
+        return len(self._time_series)
 
     def __getitem__(self,index):
-        return data[index]
+        cls = type(self)
+        if isinstance(index, slice):
+            return cls(self._time_series[index])
+        elif isinstance(index, numbers.Integral): 
+            return self._time_series[index]
         
     def __setitem__(self,index,value):
-        return data[index] = value
+        self._time_series[index] = value
 
-    def __str__(self):
