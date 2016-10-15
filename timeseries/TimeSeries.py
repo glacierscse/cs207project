0dd107d3e9cc7e6983c4c8a050178e0c7817ce52
Feifei <fpeng01@g.harvard.edu>
2016-10-15 16:52:56 -0400

    fix docstring starting position

--- timeseries/TimeSeries.py
+++ timeseries/TimeSeries.py
@@ -3,25 +3,25 @@ class TimeSeries:
        The TimeSeries class can store a single, ordered set of numerical data.
     '''
     def __init__(self,data):
-	'''The constructor should take the initial sequence-like data to fill the time series.
-	'''
+	    '''The constructor should take the initial sequence-like data to fill the time series.
+	    '''
         self.timeSeries = data
 
     def __len__(self):
-	'''Get the length of the timeseries data.
-	'''
+	    '''Get the length of the timeseries data.
+	    '''
         return len(data)
 
     def __getitem__(self,index):
-	'''Get the data at the position specified by index. 
-	'''
+	    '''Get the data at the position specified by index. 
+	    '''
         return data[index]
         
     def __setitem__(self,index,value):
         '''Set the data to the input value at the position specified by index.
-	'''
+	    '''
 	return data[index] = value
 
     def __str__(self):
-    '''Return a string representation of the timeseries data
-    '''
+        '''Return a string representation of the timeseries data
+        '''
