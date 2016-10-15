ac835381821d50b3e702de44272c692af7948a51
JunZhou1544 <judyzhou0201@gmail.com>
2016-10-15 13:52:50 -0400

    new branch

--- timeseries/TimeSeries.py
+++ timeseries/TimeSeries.py
@@ -8,5 +8,5 @@ class TimeSeries:
     def __getitem__(self,index):
         return data[index]
         
-    def __setitem__(self,index,item):
-        return data[index] = item
+    def __setitem__(self,index,value):
+        return data[index] = value
