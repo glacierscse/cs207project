e5b29cfc6339a257c8f22b117153591f4425acfe
JunZhou1544 <judyzhou0201@gmail.com>
2016-10-15 13:27:36 -0400

    first commit for timeseries folder and class

---
+++ timeseries/TimeSeries.py
@@ -0,0 +1,10 @@
+class TimeSeries:
+    def __init__(self,data):
+        self.timeSeries = data
+
+    def __len__(self):
+        return len(data)
+
+    def __getitem__():
+
+    def __setitem__():
