20d9d9484cc9078bc7b49357d1e1f0d6dee9b6b2
JunZhou1544 <judyzhou0201@gmail.com>
2016-10-20 19:30:11 -0400

    changed init, len, getitem, setitem. added iter related functions

--- timeseries/TimeSeries.py
+++ timeseries/TimeSeries.py
@@ -1,21 +1,26 @@
 import numbers
 from reprlib import recursive_repr
-
-
+import numpy as np
 class TimeSeries:
     '''This is the TimeSeries class implemented using Python.
        The TimeSeries class can store a single, ordered set of numerical data.
     '''
-    def __init__(self,data):
+    def __init__(self, data, time = None):
         '''The constructor should take the initial sequence-like data to fill the time series.
            The sequence-like data can have length 0, but must be given.
         '''
-        self._time_series = data
+        len_data = len(data)
+        if time == None:
+            self._key = list(range(len_data))
+        else:
+            self._key = time
+        self._value = data
+        self._time_series = list(zip(self._key, self._value))
     
     def __len__(self):
         '''Get the length of the timeseries data.
         '''
-        return len(self._time_series)
+        return len(self._value)
 
     def __getitem__(self,index):
         '''Get the data at the position specified by index. 
@@ -26,15 +31,45 @@ class TimeSeries:
         elif isinstance(index, numbers.Integral):
             return self._time_series[index]
     
-    def __setitem__(self,index,value):
+    def __setitem__(self, index, pair):
         '''Set the data to the input value at the position specified by index.
         '''
-        self._time_series[index] = value
+        self._time_series[index] = pair
 
     @recursive_repr()
+    def __repr__(self):
         '''Return formal string representation of the timeseries data.
         '''
-    def __repr__(self):
         class_name = type(self).__name__
         return class_name + '(' + ', '.join(map(repr, self)) + ')'
+        
+    def __str__(self):
+        limit_len = 5
+        len_data = len(self._time_series)
+        if len_data <= limit_len:
+            return str(self._time_series)
+        else:
+            str_part1 = ", ".join(str(item) for item in self._time_series[0:limit_len])
+            return "[" + str_part1 + " ... " + str(self._time_series[-1]) + "]" 
+
+    def __iter__(self):
+        for val in self._value:
+            yield val
+
+    def itervalues(self):
+        for val in self._value:
+            yield val
+
+    def itertimes(self):
+        for time in self._key:
+            yield time
+
+    def iteritems(self):
+        for item in self._time_series:
+            yield item
+
+    #def values(self):
+     #   return np.array(self._value)
+
+
 
